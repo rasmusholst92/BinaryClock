a1097f6a218eff073068428cb4f045d5fb3e648d
rasmusholst92 <rasmus-holst@outlook.com>
2023-05-11 13:03:06 +0200

    Oskar merge

--- binary_clock.py
+++ binary_clock.py
@@ -1,5 +1,6 @@
 from sense_hat import SenseHat, ACTION_PRESSED
 import time
+import argparse
 
 sense = SenseHat()
 
@@ -17,15 +18,77 @@ def set_12_hour(event):
     '''Koden for 12 timers ur'''
     if event.action == ACTION_PRESSED:
         global is_24_hour
+        global am_pm
         is_24_hour = False
+        am_pm = True
         sense.show_letter("1")
 
 def set_rotation(event):
     '''Skifter rotationen mellem 0 & 90 grader'''
     global rotation
+    global vertical
     if event.action == ACTION_PRESSED:
         rotation = 0 if rotation == 90 else 90
+        vertical = True
         sense.rotation = rotation
+
+def dec_to_bin(value):
+    binary = bin(value)[1:].zfill(8)
+    return binary
+
+def get_binary_time(am_pm):
+    current_time = time.strftime("%H:%M:%S")
+    hour, minute, second = current_time.split(":")
+    hour = int(hour)
+    if (am_pm):
+        if hour >= 12:
+            if hour > 12:
+                hour -=12
+            if hour == 0:
+                hour = 12
+    hour = 12
+    binary_hour = dec_to_bin(int(hour))
+    binary_minute = dec_to_bin(int(minute))
+    binary_second = dec_to_bin(int(second))
+    binary_time = binary_hour + binary_minute + binary_second
+    return binary_time
+
+def display(vertical, am_pm):
+    binary_time = get_binary_time(am_pm)
+    for x in range(8):
+        for y in range(8):
+            pixel_index = x * 8 + y
+            if(pixel_index > 23):
+                continue
+            if binary_time[pixel_index] == "1":
+                if(vertical):
+                    sense.set_pixel(x,y(255,255,255))
+                else:
+                    sense.set_pixel(y,x(255,255,255))
+            else:
+                if(vertical):
+                    sense.set_pixel(x,y(0,0,0))
+                else:
+                    sense.set_pixel(y,x(0,0,0))
+
+sense.clear()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--vertical', type=str, help="Diplay vertical")
+parser.add_argument('--am_pm', type=str, help="Display 12-hour format")
+args = parser.parse_args()
+
+vertical = False
+am_pm = False
+
+if (args.vertical.lower() == 'true'):
+    vertical = True
+
+if (args.am_pm.lower() == 'true'):
+    am_pm = True
+
+while (vertical | am_pm):
+    display(vertical, am_pm)
 
 
 while True:
@@ -35,3 +98,7 @@ while True:
         sense.stick.direction_right = set_24_hour
         sense.stick.direction_left = set_12_hour
         sense.stick.direction_up = set_rotation
+        display(vertical, am_pm)
+
+
+
