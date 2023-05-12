d892a965cf145669deb773e85b99a0f10be0b4a0
rasmusholst92 <rasmus-holst@outlook.com>
2023-05-12 08:46:22 +0200

    Clean up

--- binary_clock.py
+++ binary_clock.py
@@ -1,120 +1,106 @@
 from sense_hat import SenseHat, ACTION_PRESSED
-import time
-import argparse
+import time, argparse
 
 sense = SenseHat()
 
-is_24_hour = None
-rotation = 0
+try:
 
-OFF = [0, 0, 0]
-RED = [255, 0, 0]
-GREEN = [0, 255, 0]
-BLUE = [0, 0, 255]
+    is_24_hour = None
+    rotation = 0
+    vertical = False
+    am_pm = False
+
+    OFF = [0, 0, 0]
+    RED = [255, 0, 0]
+    GREEN = [0, 255, 0]
+    BLUE = [0, 0, 255]
 
 
-def set_24_hour(event):
-    '''Koden for 24 timers ur'''
-    global is_24_hour
-    if event.action == ACTION_PRESSED:
-        is_24_hour = True
+    def dec_to_bin(value):
+        binary = bin(value)[1:].zfill(8)
+        return binary
+
+    def get_binary_time(am_pm):
+        current_time = time.strftime("%H:%M:%S")
+        hour, minute, second = current_time.split(":")
+        hour = int(hour)
+        if (am_pm):
+            if hour >= 12:
+                if hour > 12:
+                    hour -=12
+                if hour == 0:
+                    hour = 12
+        binary_hour = dec_to_bin(int(hour))
+        binary_minute = dec_to_bin(int(minute))
+        binary_second = dec_to_bin(int(second))
+        binary_time = binary_hour + binary_minute + binary_second
+        return binary_time
+
+    def display(vertical, am_pm):
+        binary_time = get_binary_time(am_pm)
+        for x in range(8):
+            for y in range(8):
+                pixel_index = x * 8 + y
+                if(pixel_index > 23):
+                    continue
+                if binary_time[pixel_index] == "1":
+                    if(vertical):
+                        sense.set_pixel(x,y,(255,255,255))
+                    else:
+                        sense.set_pixel(y,x,(255,255,255))
+                else:
+                    if(vertical):
+                        sense.set_pixel(x,y,(0,0,0))
+                    else:
+                        sense.set_pixel(y,x,(0,0,0))
+
+    def change_rotation(event):
+        '''Skifter rotationen mellem 0 & 90 grader'''
+        global rotation
+        if event.action == ACTION_PRESSED:
+            rotation = 0 if rotation == 90 else 90
+            sense.rotation = rotation
+            sense.clear()
+
+    def set_rotation(event):
         global vertical
-        vertical = False
+        if(event.action == ACTION_PRESSED):
+            vertical = not vertical
+            sense.clear()
 
-def set_12_hour(event):
-    '''Koden for 12 timers ur'''
-    if event.action == ACTION_PRESSED:
-        global is_24_hour
+    def set_24_hour(event):
         global am_pm
-        is_24_hour = False
-        am_pm = True
+        if(event.action == ACTION_PRESSED):
+            am_pm = False
+            sense.clear()
 
-def set_rotation(event):
-    '''Skifter rotationen mellem 0 & 90 grader'''
-    global rotation
-    global vertical
-    if event.action == ACTION_PRESSED:
-        rotation = 0 if rotation == 90 else 90
+    def set_12_hour(event):
+        global am_pm
+        if(event.action == ACTION_PRESSED):
+            am_pm = True
+            sense.clear()
+
+
+    sense.clear()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--vertical', type=str, default="false", help="Diplay vertical")
+    parser.add_argument('--am_pm', type=str,default="false", help="Display 12-hour format")
+    args = parser.parse_args()
+
+    if (args.vertical.lower() == 'true'):
         vertical = True
-        sense.rotation = rotation
-        sense.clear()
 
-def dec_to_bin(value):
-    binary = bin(value)[1:].zfill(8)
-    return binary
-
-def get_binary_time(am_pm):
-    current_time = time.strftime("%H:%M:%S")
-    hour, minute, second = current_time.split(":")
-    hour = int(hour)
-    if (am_pm):
-        if hour >= 12:
-            if hour > 12:
-                hour -=12
-            if hour == 0:
-                hour = 12
-    binary_hour = dec_to_bin(int(hour))
-    binary_minute = dec_to_bin(int(minute))
-    binary_second = dec_to_bin(int(second))
-    binary_time = binary_hour + binary_minute + binary_second
-    return binary_time
-
-def display(vertical, am_pm):
-    binary_time = get_binary_time(am_pm)
-    for x in range(8):
-        for y in range(8):
-            pixel_index = x * 8 + y
-            if(pixel_index > 23):
-                continue
-            if binary_time[pixel_index] == "1":
-                if(vertical):
-                    sense.set_pixel(x,y,(255,255,255))
-                else:
-                    sense.set_pixel(y,x,(255,255,255))
-            else:
-                if(vertical):
-                    sense.set_pixel(x,y,(0,0,0))
-                else:
-                    sense.set_pixel(y,x,(0,0,0))
-
-def r(event):
-    global vertical
-    if(event.action == ACTION_PRESSED):
-        vertical = not vertical
-        sense.clear()
-
-def a(event):
-    global am_pm
-    if(event.action == ACTION_PRESSED):
-        am_pm = False
-        sense.clear()
-
-def p(event):
-    global am_pm
-    if(event.action == ACTION_PRESSED):
+    if (args.am_pm.lower() == 'true'):
         am_pm = True
-        sense.clear()
 
-sense.clear()
+    while True:
+            time.sleep(0.1)
+            sense.stick.direction_up = set_rotation
+            sense.stick.direction_right = set_24_hour
+            sense.stick.direction_left = set_12_hour
+            sense.clear()
+            display(vertical, am_pm)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--vertical', type=str, default="false", help="Diplay vertical")
-parser.add_argument('--am_pm', type=str,default="false", help="Display 12-hour format")
-args = parser.parse_args()
-
-vertical = False
-am_pm = False
-
-if (args.vertical.lower() == 'true'):
-    vertical = True
-
-if (args.am_pm.lower() == 'true'):
-    am_pm = True
-
-while True:
-        time.sleep(0.1)
-        sense.stick.direction_up = r
-        sense.stick.direction_right = a
-        sense.stick.direction_left = p
-        sense.clear()
-        display(vertical, am_pm)
+except KeyboardInterrupt:
+    sense.show_message("Programmet slutter", scroll_speed = 0.06, text_colour = [255, 0, 0])
