dd0f77457f82189571a7e5e7ae924a4b7629560d
MartinMidtgaard <martinmidtgaard@yahoo.com>
2023-05-15 12:36:49 +0200

    kommentarer

--- binary_clock.py
+++ binary_clock.py
@@ -5,36 +5,39 @@ from flask import Flask, jsonify
 # pandoc binary_clock.1.md -s -t man -o binary_clock.1
 # man -l binary_clock.1
 
+# Handler til at kunne stoppe servicen via ^C  eller et eksternt terminal vindue
 def term_handler(signum, frame):
     '''SIGTERM til nedlukning af service udefra'''
-    sense.show_message("Programmet slutter", scroll_speed = 0.04, text_colour = [255, 0, 0])
+    sense.show_message("Programmet slutter", scroll_speed = 0.04, text_colour = [255, 0, 0]) # Slut besked
     print("SIG INT/TERM activated")
     sys.exit(0)
 
-signal.signal(signal.SIGTERM, term_handler)
-signal.signal(signal.SIGINT, term_handler)
+signal.signal(signal.SIGTERM, term_handler) # SIGTERM for lukning via terminal (sudo kill)
+signal.signal(signal.SIGINT, term_handler)  # SIGINT for lukning via control C (^C)
 
 sense = SenseHat()
 
-sense.show_message("Programmet starter", scroll_speed=0.04, text_colour=[0,255,0])
+sense.show_message("Programmet starter", scroll_speed=0.04, text_colour=[0,255,0]) # Start besked
 
-vertical = False
-am_pm = False
-TIME_SEGMENT = 8
-MINUTE_SEGMENT = 16
-SECOND_SEGMENT = 24
+vertical = False        # Global til vertikal og horisontal visning
+am_pm = False           # Global til skiftning af 24 og 12 timer ur
+TIME_SEGMENT = 8        # Global til timer
+MINUTE_SEGMENT = 16     # Global til minutter
+SECOND_SEGMENT = 24     # Global til sekunder
 
-# Farver til pixels.
+# Farver til pixels
 OFF = [0, 0, 0]
 RED = [255, 0, 0]   # Second
 BLUE = [0, 0, 255]  # Minute
 GREEN = [0, 255, 0] # Hour
 
+# Decimaler konventeres til binært (det er jo et binært ur)
 def dec_to_bin(value):
     '''Konventerer decimaler til binært'''
     binary = bin(value)[1:].zfill(8)
     return binary
 
+# Opsætning af hvordan uret skal kører og vise tiden
 def get_binary_time(am_pm):
     '''Hentning af tid i binært'''
     hour, minute, second = time.localtime()[3:6]
@@ -51,6 +54,7 @@ def get_binary_time(am_pm):
     binary_time = binary_hour + binary_minute + binary_second
     return binary_time
 
+# Hvordan uret skal fremvises på RaspberryPi SenseHat modulet
 def display(vertical: bool, am_pm: bool):
     '''Display af uret på SenseHat'''
     binary_time = get_binary_time(am_pm)
@@ -62,33 +66,36 @@ def display(vertical: bool, am_pm: bool):
             color = OFF
             if binary_time[pixel_index] == "1":
                 if pixel_index < TIME_SEGMENT:
-                    color = RED
+                    color = RED # Sekunder vises i rødt
                 elif pixel_index < MINUTE_SEGMENT:
-                    color = BLUE
+                    color = BLUE # Minuter vises i blår
                 else:
-                    color = GREEN
+                    color = GREEN # Timer vises i grønt
                 if(vertical):
                     sense.set_pixel(x,y,color)
                 else:
                     sense.set_pixel(y,x,color)
 
+# Rotations eventet
 def set_rotation(event):
     '''Rotation af uret på Sense Hat'''
     global vertical
     if(event.action == ACTION_PRESSED):
         vertical = not vertical
 
+# 24 timer eventet
 def set_24_hour(event):
     '''Sætter uret til 24 timer visning'''
     global am_pm
     if(event.action == ACTION_PRESSED):
-        am_pm = False
+        am_pm = False # Hvis false er den på 24 timers visning
 
+# 12 timer eventet
 def set_12_hour(event):
     '''Sætter uret til 12 timer visning'''
     global am_pm
     if(event.action == ACTION_PRESSED):
-        am_pm = True
+        am_pm = True # Hvis true er den på 12 timers visning
 
 
 sense.clear()
@@ -105,7 +112,7 @@ if (args.am_pm.lower() == 'true'):
     am_pm = True
 
 
-# GET request der viser tempratur og luftfugtighed.
+# GET request der viser temperatur og luftfugtighed
 app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def sensehat_data():
@@ -118,6 +125,7 @@ def sensehat_data():
     }
     return jsonify(data)
 
+# Main så man-page kunne fungerer
 def main():
     '''
     ENTRY POINT TO THE PROGRAM
