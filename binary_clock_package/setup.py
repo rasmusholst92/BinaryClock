936f723dc89fa600869b8a85d4357fdf95207574
GieraryHirr <oskarkulpa99@gmail.com>
2023-05-15 10:51:44 +0200

    Modul opg, virker ikke

---
+++ binary_clock_package/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bin_clock',
+    version='1.0',
+    author='Rasmus, Martin, Oskar',
+    description='Et binært ur',
+    packages=find_packages(),
+    install_requires=['sense_hat'],
+    entry_points={
+        'console_scripts': [
+            'bin_clock = bin_clock.clock:main'
+        ]
+    }
+)
